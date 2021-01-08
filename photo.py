0c9c3243f1bd288100317549e5e727de59fc3834
dieudv <dieu.cit@gmail.com>
2021-01-08 20:50:30 +0700

    Fix missing schema

--- photo.py
+++ photo.py
@@ -6,6 +6,7 @@ cookies = {
 }
 friend_id = ""
 
+host = "https://mbasic.facebook.com"
 
 def load_photo(input_url):
     req = requests.Session()
@@ -19,16 +20,18 @@ def load_photo(input_url):
         if match:
             for m in match:
                 f = open("{}/{}.jpg".format(friend_id, m),"wb")
-                res = req.get("https://mbasic.facebook.com/photo/view_full_size/?fbid={}".format(m), cookies=cookies)
+                res = req.get("{}/photo/view_full_size/?fbid={}".format(host, m), cookies=cookies)
                 html = res.text
                 z = re.search(r"a href=\"(.*?)\"", html)
                 if z:
                     url = str(z.groups()[0]).replace("&amp;", "&")
+                    if "https://" not in url:
+                        continue
                     res = req.get(url, cookies=cookies)
                     f.write(res.content)
                     f.close()
                 else:
-                    break
+                    continue
             offset+=12
             print(offset)
         else:
@@ -39,8 +42,8 @@ if __name__ == "__main__":
     if not os.path.exists(friend_id):
         os.makedirs(friend_id)
 
-    url_photo_tag = "https://mbasic.facebook.com/{}/photoset/pb.{}.-2207520000../?owner_id={}&offset=".format(friend_id, friend_id, friend_id)
-    url_photo_upload = "https://mbasic.facebook.com/{}/photoset/t.{}/?owner_id={}&offset=".format(friend_id, friend_id, friend_id)
+    url_photo_tag = "{}/{}/photoset/pb.{}.-2207520000../?owner_id={}&offset=".format(host, friend_id, friend_id, friend_id)
+    url_photo_upload = "{}/{}/photoset/t.{}/?owner_id={}&offset=".format(host, friend_id, friend_id, friend_id)
 
     load_photo(url_photo_tag)
     load_photo(url_photo_upload)
